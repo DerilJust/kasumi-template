34fd08ec55833c27f57e97ab7295ccc7f37360a9
Deril <443123790@qq.com>
2025-08-30 09:39:07 +0800

    no context memory chat and stream response

--- src/command/ask.ts
+++ src/command/ask.ts
@@ -16,23 +16,24 @@ class Ask extends BaseCommand {
             const askContent = session.args.join(' ');
             client.logger.info("User question:", askContent);
             const stream = await openai.chat.completions.create({
-                messages: [{ role: "system", content: "你是一个聪明又可爱的助手，请用在番剧Mygo和Ave Mujica中千早爱音的风格回答我的问题" }, { role: "user", content: askContent }],
+                messages: [{ role: "system", content: "你是一个聪明又可爱的助手，请模仿在番剧Mygo和Ave Mujica中千早爱音(Anon Chihaya)的风格并使用中文回答我的问题" }, { role: "user", content: askContent }],
                 model: "deepseek-reasoner",
                 stream: true,
             });
-            for await (const chunk of stream) {
-                const content = chunk.choices?.[0]?.delta?.content;
-                client.logger.info('AI answer:', content);
-                if (content) process.stdout.write(content);
+            const { data } = await session.send("正在向AI提问，请稍候...");
+            if (data) {
+                const messageId = data.msg_id;
+                const stringBuilder: string[] = [];
+                for await (const chunk of stream) {
+                    const content = chunk.choices?.[0]?.delta?.content;
+                    if (content) {
+                        stringBuilder.push(content);
+                        session.update(messageId, stringBuilder.join(''));
+                        client.logger.info('AI answer:', content);
+                    }
+                }
+                session.update(messageId, stringBuilder.join(''));
             }
-            // const answer = completion.choices[0].message.content || "对不起，我无法回答你的问题。";
-            // client.logger.info('AI answer:', answer);
-            // const { err, data } = await session.send(answer);
-            // if (err) {
-            //     client.logger.error('Error sending message:', err);
-            // } else {
-
-            // }
         }
     }
 }
